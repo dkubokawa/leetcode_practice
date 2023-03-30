3cea0bbecb001dde2165991add33108ca4b30303
dkubokawa <107090710+dkubokawa@users.noreply.github.com>
2023-03-29 23:04:42 -0700

    Add new tests for LC36, LC49, LC347.

--- tests/test_arrays_and_hashing.py
+++ tests/test_arrays_and_hashing.py
@@ -1,6 +1,11 @@
 from arrays_and_hashing.contains_duplicate_217 import Solution as contains_duplicate
+from arrays_and_hashing.group_anagrams_49 import Solution as group_anagrams
+from arrays_and_hashing.top_k_frequent_elements_347 import (
+    Solution as top_k_frequent_elements,
+)
 from arrays_and_hashing.two_sum_1 import Solution as two_sum
 from arrays_and_hashing.valid_anagram_242 import Solution as valid_anagram
+from arrays_and_hashing.valid_sudoku_36 import Solution as valid_sudoku
 
 
 def test_two_sum_simple():
@@ -31,3 +36,70 @@ def test_contains_duplicate_complex():
     assert True == contains_duplicate().containsDuplicate(
         [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
     )
+
+
+def test_valid_sudoku_simple():
+    assert True == valid_sudoku().isValidSudoku(
+        [
+            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+        ]
+    )
+    assert False == valid_sudoku().isValidSudoku(
+        [
+            ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+        ]
+    )
+
+
+def test_is_valid_sudoku_complex():
+    assert False == valid_sudoku().isValidSudoku(
+        [
+            [".", ".", ".", ".", "5", ".", ".", "1", "."],
+            [".", "4", ".", "3", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", "3", ".", ".", "1"],
+            ["8", ".", ".", ".", ".", ".", ".", "2", "."],
+            [".", ".", "2", ".", "7", ".", ".", ".", "."],
+            [".", "1", "5", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", "2", ".", ".", "."],
+            [".", "2", ".", "9", ".", ".", ".", ".", "."],
+            [".", ".", "4", ".", ".", ".", ".", ".", "."],
+        ]
+    )
+
+
+def test_group_anagrams_simple():
+    results = group_anagrams().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+    assert ["eat", "tea", "ate"] in results
+    assert ["tan", "nat"] in results
+    assert ["bat"] in results
+
+
+def test_group_anagrams_complex():
+    assert [["a"]] == group_anagrams().groupAnagrams(["a"])
+    assert [[""]] == group_anagrams().groupAnagrams([""])
+
+
+def test_top_k_frequent_elements_simple():
+    assert [1, 2] == top_k_frequent_elements().topKFrequent([1, 1, 1, 2, 2, 3], 2)
+    assert [1, 2, 3] == top_k_frequent_elements().topKFrequent([1, 1, 1, 2, 2, 3], 3)
+    assert [1] == top_k_frequent_elements().topKFrequent([1], 1)
+
+
+def test_top_k_frequent_elements_complex():
+    assert [1, 2] == top_k_frequent_elements().topKFrequent([1, 2], 2)
