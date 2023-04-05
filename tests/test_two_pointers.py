5e32510308baeb1e9aa354c6ff0b4c33c0154b31
dkubokawa <107090710+dkubokawa@users.noreply.github.com>
2023-04-05 13:39:36 -0700

    LC167 (Med) Two Sum Part Two Sorted Input Array (#11)
    Implementation:
    
    - We use two pointers, one at the beginning and one at the end. We add
    the two numbers.
    - If it is less than the target, we move the left pointer to the right.
    - If it is greater than the target, we move the right pointer to the
    left.
    - We keep doing this until we find the two numbers that add up to the
    target.
    - We return the indices of the two numbers.
    - Time Complexity: O(n) since we will only iterate through the array
    once
    - Space Complexity: O(1) since we are not using any extra space

--- tests/test_two_pointers.py
+++ tests/test_two_pointers.py
@@ -1,3 +1,4 @@
+from two_pointers.two_sum_2_input_array_is_sorted_167 import Solution as two_sum_2
 from two_pointers.valid_palindrome_125 import Solution as valid_palindrome
 
 
@@ -10,3 +11,17 @@ def test_valid_palindrome_simple():
 
     s3 = " "
     assert True == valid_palindrome().isPalindrome(s3)
+
+
+def test_two_sum_2_simple():
+    numbers = [2, 7, 11, 15]
+    target = 9
+    assert [1, 2] == two_sum_2().twoSum(numbers, target)
+
+    numbers = [2, 3, 4]
+    target = 6
+    assert [1, 3] == two_sum_2().twoSum(numbers, target)
+
+    numbers = [-1, 0]
+    target = -1
+    assert [1, 2] == two_sum_2().twoSum(numbers, target)
---
+++ two_pointers/two_sum_2_input_array_is_sorted_167.py
@@ -0,0 +1,68 @@
+"""
+Given a 1-indexed array of integers numbers that is already sorted in non-
+decreasing order, find two numbers such that they add up to a specific target
+number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1
+ < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an
+integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not
+use the same element twice.
+Your solution must use only constant extra space.
+
+Example 1:
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We
+return [1, 2].
+
+Example 2:
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We
+return [1, 3].
+
+Example 3:
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We
+return [1, 2].
+
+
+Constraints:
+2 <= numbers.length <= 3 * 10⁴
+-1000 <= numbers[i] <= 1000
+numbers is sorted in non-decreasing order.
+-1000 <= target <= 1000
+The tests are generated such that there is exactly one solution.
+"""
+
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        """
+        :param List[int] numbers: input array
+        :param int target: the target sum
+        :return: List[int, int]. Two indices of the two numbers that add up to target.
+        Implementation:
+            We use two pointers, one at the beginning and one at the end. We add the two numbers.
+            If it is less than the target, we move the left pointer to the right.
+            If it is greater than the target, we move the right pointer to the left.
+            We keep doing this until we find the two numbers that add up to the target.
+            We return the indices of the two numbers.
+        Time Complexity: O(n) since we will only iterate through the array once
+        Space Complexity: O(1) since we are not using any extra space
+        """
+
+        left = 0
+        right = len(numbers) - 1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                # Leetcode wants the index to start at 1. So we add 1 to both.
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                right -= 1
