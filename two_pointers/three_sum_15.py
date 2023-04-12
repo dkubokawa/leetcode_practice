a771e68ef389503c4004b2a98f794766012b0aaf
dkubokawa <107090710+dkubokawa@users.noreply.github.com>
2023-04-11 18:55:57 -0700

    Update documentation and define time and space complexity.

--- two_pointers/three_sum_15.py
+++ two_pointers/three_sum_15.py
@@ -35,6 +35,21 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        """
+        :param List[int] nums: input array
+        :return: List[List[int]]. All triplets that add up to 0.
+        Implementation:
+            We sort the array.
+            We iterate through the array, and for each element, we use two pointers
+            to find the other two numbers that add up to 0.
+            We use a list to keep track of the triplets we have already seen.
+            We return the list of triplets.
+        Time Complexity: O(nlogn) + O(n^2) = O(n^2) since we will iterate through the array once, and for each
+            element, we will iterate through the rest of the array using two pointers.
+        Space Complexity: O(n) since we are using a list to keep track of the triplets.
+        """
+
+
         if len(nums) < 2:
             return []
 
