9cd72f04b696d51179681dbf28408df524b58dfc
dkubokawa <107090710+dkubokawa@users.noreply.github.com>
2023-04-27 15:40:47 -0700

    LC424 (Medium) : Longest Repeating Character Replacement (#19)
    Implementation:
    1. Use a hashmap to store the number of times a character appears in the
    window
    2. Use a left and right pointer to keep track of the window
    3. Use a variable to keep track of the most frequent character in the
    window
    4. If the window is not valid, move the left pointer and decrement the
    value of the leftmost character
    5. Return the length of the window
    
    Time Complexity: O(n) since we are iterating through the string once
    Space Complexity: O(26) = O(1) since the hashmap will only contain 26
    characters at most

--- tests/test_sliding_window.py
+++ tests/test_sliding_window.py
@@ -1,4 +1,7 @@
 from sliding_window.best_time_to_buy_stock_121 import Solution as best_time_to_buy_stock
+from sliding_window.longest_repeating_character_replacement_424 import (
+    Solution as longest_repeating_character_replacement,
+)
 from sliding_window.longest_substring_without_repeating_characters_3 import \
     Solution as longest_substring_without_repeating_characters
 
@@ -6,6 +9,30 @@ from sliding_window.longest_substring_without_repeating_characters_3 import \
 def test_best_time_to_buy_stock_simple():
     assert 5 == best_time_to_buy_stock().maxProfit([7, 1, 5, 3, 6, 4])
     assert 0 == best_time_to_buy_stock().maxProfit([7, 6, 4, 3, 1])
+
+
+def test_longest_repeating_character_replacement_simple():
+    assert 4 == longest_repeating_character_replacement().characterReplacement(
+        "ABAB", 2
+    )
+    assert 4 == longest_repeating_character_replacement().characterReplacement(
+        "AABABBA", 1
+    )
+    assert 2 == longest_repeating_character_replacement().characterReplacement(
+        "AABABBA", 0
+    )
+
+
+def test_longest_repeating_character_replacement_complex():
+    assert 2 == longest_repeating_character_replacement().characterReplacement(
+        "ABCD", 1
+    )
+    assert 3 == longest_repeating_character_replacement().characterReplacement(
+        "ABCD", 2
+    )
+    assert 4 == longest_repeating_character_replacement().characterReplacement(
+        "ABCD", 3
+    )
 
 
 def test_longest_substring_without_repeating_characters_simple():
