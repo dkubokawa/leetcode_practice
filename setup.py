c62e2e31cb895b4fd741281a8fdb159a6ca5b4b9
dkubokawa <107090710+dkubokawa@users.noreply.github.com>
2023-05-01 16:09:17 -0700

    Update the setup file to include additional packages.

--- setup.py
+++ setup.py
@@ -5,3 +5,18 @@ setup(
     version='0.0.0',
     packages=find_packages(),
 )
+setup(
+    name='sliding_window',
+    version='0.0.0',
+    packages=find_packages(),
+)
+setup(
+    name='two_pointers',
+    version='0.0.0',
+    packages=find_packages(),
+)
+setup(
+    name='stack',
+    version='0.0.0',
+    packages=find_packages(),
+)
