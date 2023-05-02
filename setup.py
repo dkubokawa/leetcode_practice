b7743b777050f697b5e3f54d230a0787755f411a
dkubokawa <107090710+dkubokawa@users.noreply.github.com>
2023-05-02 13:20:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='arrays_and_hashing',
-    version='0.0.0',
     packages=find_packages(),
-)
-setup(
-    name='sliding_window',
-    version='0.0.0',
-    packages=find_packages(),
-)
-setup(
-    name='two_pointers',
-    version='0.0.0',
-    packages=find_packages(),
-)
-setup(
-    name='stack',
-    version='0.0.0',
-    packages=find_packages(),
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
 )
