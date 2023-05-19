dedf8b4f3258e044dddae8409bfa1718549b5a22
dkubokawa <dkubokawa@g.ucla.edu>
2023-05-19 12:14:36 -0700

    Syntax

--- sql/employee_bonus_577.sql
+++ sql/employee_bonus_577.sql
@@ -55,8 +55,8 @@ Output:
 """
 
 SELECT e.name, b.bonus
-FROM Employee e
-LEFT JOIN Bonus b
-ON e.empID = b.empID
-WHERE b.bonus < 1000
-OR b.bonus is NULL
+  FROM Employee e
+  LEFT JOIN Bonus b
+       ON e.empID = b.empID
+       WHERE b.bonus < 1000
+       OR b.bonus is NULL
