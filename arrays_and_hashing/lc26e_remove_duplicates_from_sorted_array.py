66733a41b6877c0f6658fc63e0cf5f32ec82b376
dkubokawa <dkubokawa@g.ucla.edu>
2023-11-10 21:34:55 -0800

    Add Time and Space complexity

--- arrays_and_hashing/lc26e_remove_duplicates_from_sorted_array.py
+++ arrays_and_hashing/lc26e_remove_duplicates_from_sorted_array.py
@@ -42,6 +42,13 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        :param List[List[str]] nums: sorted array of integers
+        :return: int: number of unique elements in nums
+        Time Complexity: O(N) since we iterate over nums once
+        Space Complexity: O(1) since we don't use any extra space
+        """
+
         i = 1
         while i < len(nums):
             if nums[i] == nums[i - 1]:
