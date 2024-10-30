538c2dabf9f2d39fefff62be243f664e88aaac8b
dkubokawa <dkubokawa@g.ucla.edu>
2024-10-29 23:32:57 -0700

    LC0226E: Invert Binary Tree

---
+++ python/tree/easy/0226e-invert-binary-tree.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # Recursive Solution
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+    def invertTreeBFS(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # BFS use deque or queue
+        queue = collections.deque([root])
+        while queue:
+            node = queue.popleft()
+            if node:
+                node.left, node.right = node.right, node.left
+                queue.append(node.left)
+                queue.append(node.right)
+        return root
+
+    def invertTreeDFS(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # Use a Stack for DFS
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack.extend([node.right, node.left])
+        return root
