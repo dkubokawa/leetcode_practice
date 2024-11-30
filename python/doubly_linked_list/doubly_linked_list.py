f93a6191ff93e434d9e3aa70d469123009d5294f
dkubokawa <107090710+dkubokawa@users.noreply.github.com>
2024-11-29 16:39:33 -0800

    Initial implementation: Doubly Linked List (DLL) (#137)
    Implement print_list, append, pop, prepend, pop_first

---
+++ python/doubly_linked_list/doubly_linked_list.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, value: int):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        tmp = self.head
+        while tmp is not None:
+            print(tmp.value)
+            tmp = tmp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head, self.tail = None, None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+                self.head, self.tail = None, None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
+
