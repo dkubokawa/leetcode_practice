851df1359c20e34e4e20366caa85d95a7d79633b
dkubokawa <107090710+dkubokawa@users.noreply.github.com>
2025-01-14 14:09:29 -0800

    Initial Implementation: MyStack (#152)

---
+++ python/stack/stack.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class MyStack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp
